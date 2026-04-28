db488e9db0bc8d173e96d975ac9be87eb4acc905
Erwin Mario Leon Ramos <erwin@apoloenergies.es>
2026-04-28 12:56:49 -0400

    refactor environmets

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://api.apoloenergies.es/api/coexpal/v1',
+  apiUrl: 'https://api.apoloenergies.es/coexpal/api/coexpal/v1',
   auth: {
     tokenStorage: 'cookie' as const,
     refreshTokenCookie: 'apolo_rt',
