83653bad51fdc05c7b86fc7626de6a4dc7301445
Gustavo Martinez <gmartinez@apoloenergies.es>
2026-07-06 10:23:31 -0400

    feat: add change of names of documentations

--- src/app/features/dashboard/pages/users/user-detail/configs/doc-type-labels.config.ts
+++ src/app/features/dashboard/pages/users/user-detail/configs/doc-type-labels.config.ts
@@ -1,9 +1,9 @@
 export const DOC_TYPE_LABELS: Record<string, string> = {
   'DniFront': 'DNI / NIE (Frontal)',
   'DniBack': 'DNI / NIE (Reverso)',
-  'AeatCertificate': 'Certificado AEAT',
-  'SsCertificate': 'Certificado Seguridad Social',
-  'BankStatement': 'Extracto Bancario',
+  'AeatCertificate': 'Certficado de estar al corriente Agencia Tributaria actualizado',
+  'SsCertificate': 'Certificado de estar al corriente con la Seguridad Social actualizado',
+  'BankStatement': 'Certificado de titularidad bancario',
   'CifCertificate': 'Certificado CIF',
   'ConstitutionDeed': 'Escritura constitución / poderes',
   'SignedContract': 'Contrato Firmado',
